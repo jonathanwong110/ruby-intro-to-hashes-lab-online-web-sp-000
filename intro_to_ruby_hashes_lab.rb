e0c46f709895f97454b0f89fe5e1655dfe3577b5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-22 20:56:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,9 @@ def monopoly_with_third_tier
     monopoly = {
     railroads: {
      pieces: 4,
-     names: {},
+     names: {
+       reading_railroad: "mortgage_value" => "$100"
+     },
      rent_in_dollars: {
        one_piece_owned: 25,
        two_pieces_owned: 50,
